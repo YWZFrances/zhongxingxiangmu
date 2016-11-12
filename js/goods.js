fd58e6f3f9db5c04c8b3013473b713f82d3bd9dc
YWZFrances <1595642024@qq.com>
2016-11-12 21:37:45 +0800

    JSON

--- js/goods.js
+++ js/goods.js
@@ -1,164 +1,257 @@
-$(document).ready(function(){
-	
+$(document).ready(function() {
+
 	//hei===============================
-	
-//	var tuo = document.getElementById("tuo")
-////	alert(tuo.offsetLeft)
-//	tuo.onmousedown = function(e){
-//		document.onmousemove = function(e){
-//			e = e || window.event;
-//			var left = e.clientX;
-//			tuo.style.left = left-300+"px"
-//			if(tuo.offsetLeft<=0){
-//				tuo.style.left = 0
-//			}
-//			if(tuo.offsetLeft>=700){
-//				tuo.style.left = 700+"px"
-//			}
-//		}
-//	}
-//	tuo.onmouseup = function(){
-//		document.onmousemove = null;
-//		if(tuo.offsetLeft>=500 && tuo.offsetLeft<=650){
-//			$(".hei").fadeOut(500)
-//		}else{
-//			tuo.style.left = 0
-//		}
-//	}
-	
-//	head====================================================================
 
-//	logo=================================================================
-	$(".gouwuche").on("mouseover",function(){
-		$(".gouwuche").css({"background":"url(../images/gouwucheimg2.png) no-repeat 31px center white","color":"black","border-bottom":"white"})
-		$(".gouwuchediv").css("display","block")
+	//	var tuo = document.getElementById("tuo")
+	////	alert(tuo.offsetLeft)
+	//	tuo.onmousedown = function(e){
+	//		document.onmousemove = function(e){
+	//			e = e || window.event;
+	//			var left = e.clientX;
+	//			tuo.style.left = left-300+"px"
+	//			if(tuo.offsetLeft<=0){
+	//				tuo.style.left = 0
+	//			}
+	//			if(tuo.offsetLeft>=700){
+	//				tuo.style.left = 700+"px"
+	//			}
+	//		}
+	//	}
+	//	tuo.onmouseup = function(){
+	//		document.onmousemove = null;
+	//		if(tuo.offsetLeft>=500 && tuo.offsetLeft<=650){
+	//			$(".hei").fadeOut(500)
+	//		}else{
+	//			tuo.style.left = 0
+	//		}
+	//	}
+
+	//	head====================================================================
+
+	//	logo=================================================================
+	$(".gouwuche").on("mouseover", function() {
+		$(".gouwuche").css({
+			"background": "url(../images/gouwucheimg2.png) no-repeat 31px center white",
+			"color": "black",
+			"border-bottom": "white"
+		})
+		$(".gouwuchediv").css("display", "block")
 	})
-	$(".gouwuche").on("mouseout",function(){
-		$(".gouwuche").css({"background":"url(../images/gouwuche.png) no-repeat 31px center #f9c81d","color":"#767676",})
-		$(".gouwuchediv").css("display","none")
+	$(".gouwuche").on("mouseout", function() {
+		$(".gouwuche").css({
+			"background": "url(../images/gouwuche.png) no-repeat 31px center #f9c81d",
+			"color": "#767676",
+		})
+		$(".gouwuchediv").css("display", "none")
 	})
-	
-//	nav=================================================================
-$(".navdiv1").on("mouseover",function(){
-	$(".xilie").stop().animate({"height":238},300)
-})
-$(".navdiv2").on("mouseover",function(){
-	$(".jingpin").stop().animate({"height":238},300)
-})
-$(".navdiv1").on("mouseout",function(){
-	$(".xilie").stop().animate({"height":0},300)
-})
-$(".navdiv2").on("mouseout",function(){
-	$(".jingpin").stop().animate({"height":0},300)
-})
 
-$(".xiliediv1,.xiliediv3").on("mouseover",function(){
-	$(this).stop().animate({"padding-top":10},500)
-})
-$(".xiliediv1,.xiliediv3").on("mouseout",function(){
-	$(this).stop().animate({"padding-top":23},500)
-})
+	//	nav=================================================================
+	$(".navdiv1").on("mouseover", function() {
+		$(".xilie").stop().animate({
+			"height": 238
+		}, 300)
+	})
+	$(".navdiv2").on("mouseover", function() {
+		$(".jingpin").stop().animate({
+			"height": 238
+		}, 300)
+	})
+	$(".navdiv1").on("mouseout", function() {
+		$(".xilie").stop().animate({
+			"height": 0
+		}, 300)
+	})
+	$(".navdiv2").on("mouseout", function() {
+		$(".jingpin").stop().animate({
+			"height": 0
+		}, 300)
+	})
 
-var time;
-	var r=0;
+	$(".xiliediv1,.xiliediv3").on("mouseover", function() {
+		$(this).stop().animate({
+			"padding-top": 10
+		}, 500)
+	})
+	$(".xiliediv1,.xiliediv3").on("mouseout", function() {
+		$(this).stop().animate({
+			"padding-top": 23
+		}, 500)
+	})
+
+	var time;
+	var r = 0;
 	clearInterval(time);
-	time = setInterval(function(){	
+	time = setInterval(function() {
 		r++;
-		if(r==7){
-			$(".navul2").css("marginTop",0)
-			r=0
+		if(r == 7) {
+			$(".navul2").css("marginTop", 0)
+			r = 0
 		}
-		$(".navul2").animate({"marginTop":-1*20*r});
-	},2500)
-	
-	
+		$(".navul2").animate({
+			"marginTop": -1 * 20 * r
+		});
+	}, 2500)
+
 	//navp1============================================
-	$(".navp1").on("mouseover",function(){
-		$(".bannerleft").css("display","block")
+	$(".navp1").on("mouseover", function() {
+		$(".bannerleft").css("display", "block")
 	})
-	$(".navp1").on("mouseout",function(){
-		$(".bannerleft").css("display","none")
+	$(".navp1").on("mouseout", function() {
+		$(".bannerleft").css("display", "none")
 	})
-	
-$(".bannerleftdiv1").children().eq(1).css({"background":"url(../images/bannerleftdiv1img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv1").children().eq(2).css({"background":"url(../images/bannerleftdiv1img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv1").children().eq(3).css({"background":"url(../images/bannerleftdiv1img3.png) no-repeat 30px center"})
-	$(".bannerleftdiv1").children().eq(4).css({"background":"url(../images/bannerleftdiv1img4.png) no-repeat 30px center"})
-	$(".bannerleftdiv1").children().eq(5).css({"background":"url(../images/bannerleftdiv1img5.png) no-repeat 30px center"})
-	$(".bannerleftdiv1").children().eq(6).css({"background":"url(../images/bannerleftdiv1img6.png) no-repeat 30px center"})
-//}
-	
-	$(".bannerleftdiv2").children().eq(1).css({"background":"url(../images/bannerleftdiv2img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv2").children().eq(2).css({"background":"url(../images/bannerleftdiv2img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv2").children().eq(3).css({"background":"url(../images/bannerleftdiv2img3.png) no-repeat 30px center"})
-	$(".bannerleftdiv2").children().eq(4).css({"background":"url(../images/bannerleftdiv2img4.png) no-repeat 30px center"})
-	
-	$(".bannerleftdiv3").children().eq(1).css({"background":"url(../images/bannerleftdiv3img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv3").children().eq(2).css({"background":"url(../images/bannerleftdiv3img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv3").children().eq(3).css({"background":"url(../images/bannerleftdiv3img3.png) no-repeat 30px center"})
-	
-	$(".bannerleftdiv4").children().eq(1).css({"background":"url(../images/bannerleftdiv4img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv4").children().eq(2).css({"background":"url(../images/bannerleftdiv4img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv4").children().eq(3).css({"background":"url(../images/bannerleftdiv4img3.png) no-repeat 30px center"})
-	
-	$(".bannerleftdiv5").children().eq(1).css({"background":"url(../images/bannerleftdiv5img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv5").children().eq(2).css({"background":"url(../images/bannerleftdiv5img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv5").children().eq(3).css({"background":"url(../images/bannerleftdiv5img3.png) no-repeat 30px center"})
-	
-	$(".bannerleftdiv6").children().eq(1).css({"background":"url(../images/bannerleftdiv6img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv6").children().eq(2).css({"background":"url(../images/bannerleftdiv6img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv6").children().eq(3).css({"background":"url(../images/bannerleftdiv6img3.png) no-repeat 30px center"})
-	
-	$(".bannerleftdiv7").children().eq(1).css({"background":"url(../images/bannerleftdiv7img1.png) no-repeat 30px center"})
-	$(".bannerleftdiv7").children().eq(2).css({"background":"url(../images/bannerleftdiv7img2.png) no-repeat 30px center"})
-	$(".bannerleftdiv7").children().eq(3).css({"background":"url(../images/bannerleftdiv7img3.png) no-repeat 30px center"})
 
-
-$(".bannerul1 li").on("mouseover",function(){
-	$(this).css("background","#BFBFBF")
-	$(this).children("div").css("display","block").stop().animate({"width":"222px"},300)
-})
-$(".bannerul1 li").on("mouseout",function(){
-	$(this).css("background","#adeaff")
-	$(this).children("div").css("display","none").stop().animate({"width":"0"},300)
-})
-$(".banner2 img").on("mouseover",function(){
-	$(this).stop().animate({"height":"170px"},200)
-})
-$(".banner2 img").on("mouseout",function(){
-	$(this).stop().animate({"height":"163px"},200)
-})
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	$(".bannerleftdiv1").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv1img1.png) no-repeat 30px center"
 	})
+	$(".bannerleftdiv1").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv1img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv1").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv1img3.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv1").children().eq(4).css({
+		"background": "url(../images/bannerleftdiv1img4.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv1").children().eq(5).css({
+		"background": "url(../images/bannerleftdiv1img5.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv1").children().eq(6).css({
+			"background": "url(../images/bannerleftdiv1img6.png) no-repeat 30px center"
+		})
+		//}
+
+	$(".bannerleftdiv2").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv2img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv2").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv2img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv2").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv2img3.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv2").children().eq(4).css({
+		"background": "url(../images/bannerleftdiv2img4.png) no-repeat 30px center"
+	})
+
+	$(".bannerleftdiv3").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv3img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv3").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv3img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv3").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv3img3.png) no-repeat 30px center"
+	})
+
+	$(".bannerleftdiv4").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv4img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv4").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv4img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv4").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv4img3.png) no-repeat 30px center"
+	})
+
+	$(".bannerleftdiv5").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv5img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv5").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv5img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv5").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv5img3.png) no-repeat 30px center"
+	})
+
+	$(".bannerleftdiv6").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv6img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv6").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv6img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv6").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv6img3.png) no-repeat 30px center"
+	})
+
+	$(".bannerleftdiv7").children().eq(1).css({
+		"background": "url(../images/bannerleftdiv7img1.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv7").children().eq(2).css({
+		"background": "url(../images/bannerleftdiv7img2.png) no-repeat 30px center"
+	})
+	$(".bannerleftdiv7").children().eq(3).css({
+		"background": "url(../images/bannerleftdiv7img3.png) no-repeat 30px center"
+	})
+
+	$(".bannerul1 li").on("mouseover", function() {
+		$(this).css("background", "#BFBFBF")
+		$(this).children("div").css("display", "block").stop().animate({
+			"width": "222px"
+		}, 300)
+	})
+	$(".bannerul1 li").on("mouseout", function() {
+		$(this).css("background", "#adeaff")
+		$(this).children("div").css("display", "none").stop().animate({
+			"width": "0"
+		}, 300)
+	})
+	$(".banner2 img").on("mouseover", function() {
+		$(this).stop().animate({
+			"height": "170px"
+		}, 200)
+	})
+	$(".banner2 img").on("mouseout", function() {
+		$(this).stop().animate({
+			"height": "163px"
+		}, 200)
+	})
+
+	//<div class="liebiaocontent">
+	//				<dl>
+	//					<dt><img src="../images/goodscontentimg1.png"/></dt>
+	//					<dd>[现货抢购]ZET中兴 天机7 MAX(全网通)</dd>
+	//					<dd class="ddred">￥2999</dd>
+	//				</dl>
+	//			</div>
+
+	//data
+	var newStr = "";
+	$.ajax("../data/liebiao.json", {
+		success: function(data) {
+			for(var j = 0; j < data.length; j++) {
+				reloadHtml(data[j].img, data[j].name, data[j].price)
+			}
+		},
+		type: "get",
+		async: true
+	});
+
+	function reloadHtml(img, name, price) {
+		newStr += "<div class = 'liebiaocontent'>";
+		newStr += "<dl>"
+		newStr += "<dt>"
+		newStr += "<img src = '../images/goodscontentimg" + img + ".png' />"
+		newStr += "</dt>"
+		newStr += "<dd>" + name + "</dd>"
+		newStr += "<dd class ='ddred'>" + price + "</dd>"
+		newStr += "</dl>"
+		newStr += "</div>"
+		$(".liebiao").html(newStr)
+	}
+
+	//div上升
+	$(".liebiaocontent").on("mouseover", function() {
+		alert(2)
+	})
+	$(".liebiaocontent").on("mouseover", function() {
+		$(this).stop().animate({
+			"margin-top": "2px"
+		}, 200).css("boxShadow", "0px 4px 8px rgba(0,0,0,.2)")
+	})
+	$(".liebiaocontent").on("mouseout", function() {
+		$(this).stop().animate({
+			"margin-top": "12px"
+		}, 200).css("boxShadow", "none)")
+	})
+
+})
--- js/login.js
+++ js/login.js
@@ -1,30 +1,24 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-
-
-
-var yanzheng = "";
-//$(".loginp2").on("click",function(){
+	var yanzheng = "";
+	//$(".loginp2").on("click",function(){
 	yanzheng = ""
-	for(var i = 0;i<4;i++){
-		yanzheng += Math.floor(Math.random()*10)
+	for(var i = 0; i < 4; i++) {
+		yanzheng += Math.floor(Math.random() * 10)
 	}
 	$(".chuyanzhengma").html(yanzheng)
-//})
-var yanzheng = "";
-$(".loginp2").on("click",function(){
-	yanzheng = ""
-	for(var i = 0;i<4;i++){
-		yanzheng += Math.floor(Math.random()*10)
-	}
-	$(".chuyanzhengma").html(yanzheng)
-})
+		//})
+	var yanzheng = "";
+	$(".loginp2").on("click", function() {
+		yanzheng = ""
+		for(var i = 0; i < 4; i++) {
+			yanzheng += Math.floor(Math.random() * 10)
+		}
+		$(".chuyanzhengma").html(yanzheng)
+	})
 
-
-
-
-//验证======================================================================
-$("#txt3").blur(function() {
+	//验证======================================================================
+	$("#txt3").blur(function() {
 		if($("#txt3").val() == $(".chuyanzhengma").text()) {
 			$(".zaicimimadiv").css("display", "none")
 			$(".jiao3").css("display", "none")
@@ -54,67 +48,47 @@ $("#txt3").blur(function() {
 		}
 	})
 
+	$("#denglu").on("click", function() {
+		//cookie有 写得没有 不存在该用户
+		//cookie有 写得有 密码错 请输入正确的密码
+		//cookie有 写得有 密码对 验证码对 登录了
+		//	alert(2)
+		var strCookie = document.cookie;
+		var arrCookie = strCookie.split("; ");
+		for(var i = 0; i < arrCookie.length; i++) {
+			var arr = arrCookie[i].split("=");
+			console.log(arr)
+			if(arr[0] == $("#txt1").val()) {
+				if(arr[1] == $("#txt2").val()  && $("#txt3").val() == $(".chuyanzhengma").text()) {
 
+					location.replace("../index.html")
 
-$("#denglu").on("click",function(){
-	//cookie有 写得没有 不存在该用户
-	//cookie有 写得有 密码错 请输入正确的密码
-	//cookie有 写得有 密码对 验证码对 登录了
-//	alert(2)
-	var strCookie=document.cookie;
-		var arrCookie=strCookie.split("; ");
-			//出來的是一組一組的鍵值對
-			//还需要分割
-			for(var i=0;i<arrCookie.length;i++){
-	            var arr=arrCookie[i].split("=");
-	            console.log(arr)
-				if(arr[0]==$("#txt1").val()){
-					if(arr[1]==$("#txt2").val() && $("#txt3").val() == $(".chuyanzhengma").text()){
-						
-							location.replace("../index.html")
-						
-					}else{
-						alert("请输入正确的验证码")
-					}
+				} 
+				if($("#txt3").val() != $(".chuyanzhengma").text()){
+					alert("请输入正确的验证码")
 				}
-//				else{
-//					alert(2)
-//				}
-				if(arr[0]==$("#txt1").val()){
-					if(arr[1]!=$("#txt2").val()){
-						alert("密码错误")
-					}
+			}
+			//				else{
+			//					alert(2)
+			//				}
+			if(arr[0] == $("#txt1").val()) {
+				if(arr[1] != $("#txt2").val()) {
+					alert("密码错误")
 				}
-//	            if(arr[0]!=$("#txt1").val()){
-//	            //用户不存在
-//	            alert("用户名不存在");
-//	            break;
-//	            }
-//	            if(arr[0])
-			}	
-			
-//			if(arr[0]!=$("#txt1").val()){
-//				alert(2)
-//			}
-//			alert(arr)
-			
-})
+			}
+			//	            if(arr[0]!=$("#txt1").val()){
+			//	            //用户不存在
+			//	            alert("用户名不存在");
+			//	            break;
+			//	            }
+			//	            if(arr[0])
+		}
 
+		//			if(arr[0]!=$("#txt1").val()){
+		//				alert(2)
+		//			}
+		//			alert(arr)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	})
 
 })
